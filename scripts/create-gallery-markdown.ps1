5602ed16ffc0d5f01697625bda5ca28107dbf5d8
Straber <mark.andrew.strasser@gmail.com>
2023-06-17 08:09:38 -0400

    Update and rename create-galleries.ps1 to create-gallery-markdown.ps1

---
+++ scripts/create-gallery-markdown.ps1
@@ -0,0 +1,67 @@
+# Generates an image gallery markdown file, given an input directory with images.
+#
+# Usage:
+# 1. Open Powershell (any windows 10 or later computer will have it, or it can be downloaded and installed on Windows or Linux)
+# 2. Create the image gallery folder and copy image jpgs into it.
+# 3. Run this script from the *root* of this repository, passing the image gallery folder path:
+#     PS> .\create-gallery-markdown.ps1 -ImageGalleryDirectory assets\images\galleries\2020-05-30 -ImageGalleryBaseName 2020-05-30
+# 4. Copy the resulting markdown file into the right directory.
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [string] $ImageGalleryDirectory,
+    
+    [Parameter(Mandatory)]
+    [string] $ImageGalleryBaseName,  
+    
+    [Parameter()]
+    [string] $OutputMarkdownDirectory = "_launch_pictures"
+)
+
+$ErrorActionPreference = "stop"
+
+# get all image files
+$galleryImages = Get-ChildItem -Path $subdirPath -File -Name
+    | Sort-Object `
+    | Where-Object { ! ($_.Contains("_tn_")) }
+    | ForEach-Object { Join-Path $subdirPath $_ }
+    
+
+# list each image and its thumbnail in the 'gallery' section of the YAML frontmatter
+$galleryYaml = "gallery:`n"
+    
+$total = $imagePaths.Length
+$count = 0
+
+foreach ($imagePath in $imagePaths) {
+    $count += 1
+
+    # find thumbnail image using convention
+    # (could generate thumbnail using powershell, see https://mohundro.com/blog/2008/10/11/simple-powershell-script-to-generate-thumbnails)
+    $imageThumbnailPath = $imagePath -replace '\\([0-9]+)_', '\$1_tn_'
+    if (!(Test-Path -Path $imageThumbnailPath -PathType leaf)) {
+      throw "Expected thumbnail path '$imageThumbnailPath' doesnt exist!"
+    }
+
+    # GitHub Actions runs Jekyll on linux, so use linux folder separators
+    $imagePath = $imagePath.Replace("\", "/")
+    $imageThumbnailPath = $imageThumbnailPath.Replace("\", "/")
+
+    # find thumbnail image using convention
+    # (could generate thumbnail using powershell, see https://mohundro.com/blog/2008/10/11/simple-powershell-script-to-generate-thumbnails)
+    $imageThumbnailPath = $imagePath -replace '/([0-9]+)_', '/$1_tn_'
+
+    # append to string
+    $galleryYaml += " - url: `"$imagePath`"`n"
+    $galleryYaml += "   image_path: `"$imageThumbnailPath`"`n"
+    $galleryYaml += "   alt: `"Image $count of $total`"`n"
+    $galleryYaml += "   title: `"Image $count of $total`"`n"  
+}
+
+# write the file
+$galleryMarkdownFile = "($OutputMarkdownDirectory)/($galleryName)-launch-event.md"
+Write-Output "---`ntitle: $galleryName Album`ncategories:`n - launch`ntags:`n - pictures" | Out-File -FilePath $galleryMarkdownFile
+Write-Output $galleryYaml | Out-File -Append -FilePath $galleryMarkdownFile
+Write-Output "---`n" | Out-File -Append -FilePath $galleryMarkdownFile
+Write-Output "{% include gallery layout=`"third`" caption=`"$galleryName`" %}" | Out-File -Append -FilePath $galleryMarkdownFile
